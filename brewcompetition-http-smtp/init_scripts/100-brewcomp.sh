35d608ac346746078eaad1dcb98e641b9b218790
dibbs online <roach@dopegoat.com>
2022-06-25 08:44:23 +1000

    Updates to 2.5.0 beta tag

--- brewcompetition-http-smtp/init_scripts/100-brewcomp.sh
+++ brewcompetition-http-smtp/init_scripts/100-brewcomp.sh
@@ -2,14 +2,31 @@
 # various initialisations of config files
 if [ ! -e /data/database ]; then cp -pr /data.skel/database /data/ ; fi
 if [ ! -e /data/database/rando ]
-then 
+then
   head -n45 /dev/urandom | tr -cd '[:alnum:]' | fold -w30 | head -n1 > /data/database/rando
 fi
 if [ ! -e /data/logs ]; then cp -pr /data.skel/logs /data/ ; fi
 if [ ! -e /data/bcoem ]; then cp -pr /data.skel/bcoem /data/ ; fi
 
 if [ ! -e /data/custom.sh ]; then cp -pr /usr/local/bin/custom.sh /data/ ; fi
-/data/custom.sh 
+/data/custom.sh
+
+# copy new site files if they arrived in an update
+if [ ! -e /data/bcoem/site/MysqliDb.php ]; then cp /data.skel/bcoem/site/MysqliDb.php /data/bcoem/site/ ; fi
+if [ ! -e /data/bcoem/site/dbObject.php ]; then cp /data.skel/bcoem/site/dbObject.php /data/bcoem/site/ ; fi
+
+# not a config file and should be updated when available
+if [[ /data.skel/bcoem/site/bootstrap.php -nt /data/bcoem/site/bootstrap.php ]]; then
+  cp /data/bcoem/site/bootstrap.php /data/bcoem/site/bootstrap.php.bak.$(date "+%Y.%m.%d-%H.%M.%S")
+  cat /data.skel/bcoem/site/bootstrap.php >/data/bcoem/site/bootstrap.php
+fi
+if [[ /data.skel/bcoem/site/MysqliDb.php -nt /data/bcoem/site/MysqliDb.php ]]; then
+  cp /data/bcoem/site/MysqliDb.php /data/bcoem/site/MysqliDb.php.bak.$(date "+%Y.%m.%d-%H.%M.%S")
+  cat /data.skel/bcoem/site/MysqliDb.php >/data/bcoem/site/MysqliDb.php
+fi
+
+# we're a http server with a ssl proxy, strip this if it exists as it breaks barcodes
+sed -i 's|^$base_url = "http://";|$base_url = "https://";|' /var/www/html/site/config.php
 
 sed -i "s/^\$mail_default_from =.*/\$mail_default_from = '${MAIL_FROM}';/" /var/www/html/site/config.mail.php
 sed -i "s/^\$smtp_host =.*/\$smtp_host = '${SMTP_HOST}';/" /var/www/html/site/config.mail.php
--- brewcompetition-http-smtp/init_scripts/custom.sh
+++ brewcompetition-http-smtp/init_scripts/custom.sh
@@ -2,10 +2,9 @@
 # put persistant changes in here if you need them.
 
 # you can increase logging by changing this.
-# if you enable these, paypal will go to sandbox url too.
-sed -i "s/define('TESTING'.*/define('TESTING', FALSE);/" /var/www/html/paths.php 
-sed -i "s/define('DEBUG'.*/define('DEBUG', FALSE);/" /var/www/html/paths.php 
-sed -i "s/define('DEBUG_SESSION_VARS'.*/define('DEBUG_SESSION_VARS', FALSE);/" /var/www/html/paths.php 
+sed -i "s/define('TESTING'.*/define('TESTING', FALSE);/" /var/www/html/paths.php
+sed -i "s/define('DEBUG'.*/define('DEBUG', FALSE);/" /var/www/html/paths.php
+sed -i "s/define('DEBUG_SESSION_VARS'.*/define('DEBUG_SESSION_VARS', FALSE);/" /var/www/html/paths.php
 
 # disable mysql strict mode as it fails hard on quick adding participants at least with an imported database.
 cat << EOF > /etc/mysql/conf.d/disable_strict_mode.cnf
@@ -17,15 +16,3 @@ EOF
 mkdir -p /var/www/html/classes/htmlpurifier/standalone/HTMLPurifier/DefinitionCache/Serializer
 chown www-data.www-data /var/www/html/classes/htmlpurifier/standalone/HTMLPurifier/DefinitionCache/Serializer
 
-# breaks barcode check in, ssl logic in app code
-# we're a http server with a ssl proxy
-sed -i 's|^$base_url = "http://";|$base_url = "https://";|' /var/www/html/site/config.php
-
-# not a config file and should be updated when available
-if [[ /data.skel/bcoem/site/bootstrap.php -nt /data/bcoem/site/bootstrap.php ]]; then
-  cp /data/bcoem/site/bootstrap.php /data/bcoem/site/bootstrap.php.bak.$(date "+%Y.%m.%d-%H.%M.%S")
-  cat /data.skel/bcoem/site/bootstrap.php >/data/bcoem/site/bootstrap.php
-fi
-
-# QABC clubs override, too many in pull down
-# sed -i 's|^?>$|$club_array = array\("Sunshine Coast Amateur Brewers \(SCABs\)","Righteous Brewers of Townsville \(RBT\)", "Noosa Home Brew Club", "Peninsula Brewers Union \(PBU\)", "Tuns of Anarchy", "Brisbanes Orchestra of Omitted Brewers \(BOOBS\)", "Western Suburbs Amateur Wine Guild", "Toowoomba Society Of Beer Appreciation \(TooSOBA\)","Small Batch Home Brew Club","SaD Brewers","Pine Rivers Underground Brewing Society \(PUBS\)","Mackay And District \(MAD\) Brewers", "Ipswich Brewers Union \(IBU\)", "GoldCLUB", "Fraser Coast Bayside Brewers", "Central Queensland Craft Brewers \(CQCB\)", "Brisbane Brewers Club \(BBC\)", "Brisbane Amateur Beer Brewers \(BABBs\)"\); asort\($club_array\); ?>|' /var/www/html/includes/constants.inc.php
